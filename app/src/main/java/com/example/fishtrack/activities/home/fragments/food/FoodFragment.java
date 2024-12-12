6f2d534a8655a95ba7c790fb5267ea5c5303336a
GabrielBezerraDev <bezerrag363@gmail.com>
2024-12-12 18:28:58 -0400

    Adicionando uma WebView pros gráficos

--- app/src/main/java/com/example/fishtrack/activities/home/fragments/food/FoodFragment.java
+++ app/src/main/java/com/example/fishtrack/activities/home/fragments/food/FoodFragment.java
@@ -4,12 +4,15 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.fishtrack.R;
 import com.example.fishtrack.databinding.FragmentFoodBinding;
 
 
@@ -24,6 +27,24 @@ public class FoodFragment extends Fragment {
 
         binding = FragmentFoodBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+        WebView temperWebView = root.findViewById(R.id.web_view_temper);
+        WebView phWebView = root.findViewById(R.id.web_view_ph);
+
+        String graphTemper = "<iframe src=\"http://192.168.31.231:5001/embed/query/1/visualization/2?api_key=nWRtSezW08fxLS6tE5Tv4lB7VXLVOhrnaQovHZ0V&\" width=\"720\" height=\"391\"></iframe>";
+
+        String graphPh = "<iframe src=\"http://192.168.31.231:5001/embed/query/1/visualization/3?api_key=nWRtSezW08fxLS6tE5Tv4lB7VXLVOhrnaQovHZ0V&\" width=\"720\" height=\"391\"></iframe>";
+
+        temperWebView.loadData(graphTemper, "text/html","utf-8");
+
+        phWebView.loadData(graphPh, "text/html","utf-8");
+
+        temperWebView.getSettings().setJavaScriptEnabled(true);
+
+        phWebView.getSettings().setJavaScriptEnabled(true);
+
+        temperWebView.setWebChromeClient(new WebChromeClient());
+
+        phWebView.setWebChromeClient(new WebChromeClient());
 
         return root;
     }
